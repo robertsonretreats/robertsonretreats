08055f230b595a670a345950263999ef7a94b96d
robertsonretreats <35204583+robertsonretreats@users.noreply.github.com>
2018-01-22 15:41:48 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -75,3 +75,18 @@ $(document).ready(function(){
 
 	/* End of customizations */
 });
+
+
+alert('in email.js');
+$('#submit').click(function(){
+  alert('HERE');
+  jQuery.ajax({
+      type: "POST",
+      url: 'email.php',
+      dataType: 'json',
+
+      success: function (obj, textstatus) {
+                    
+              }
+  });
+});
